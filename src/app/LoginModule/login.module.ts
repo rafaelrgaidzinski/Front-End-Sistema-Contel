a3bbfbc8aeba9f0db634fdb298791243d1b927eb
Rafael RG <rafaelgaidzinski@gmail.com>
2022-10-29 20:19:21 -0300

    Realizado a criação do HTML e CSS da tela de cadastro de novo usuário do sistema Contel

--- src/app/LoginModule/login.module.ts
+++ src/app/LoginModule/login.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from "@angular/core";
+
 import { LoginComponent } from "./login/login.component";
+import { UserRegisterComponent } from './user-register/user-register.component';
 
 @NgModule({
 
     declarations: [  
-        LoginComponent
+        LoginComponent, 
+        UserRegisterComponent
     ], 
 
     exports: [
-        LoginComponent
+        LoginComponent,
+        UserRegisterComponent
     ]
 })
 export class LoginModule{ }
