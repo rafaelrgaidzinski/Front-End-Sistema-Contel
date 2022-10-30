5106e0b9b373648f5714df25f57c0fb47360ba04
Rafael RG <rafaelgaidzinski@gmail.com>
2022-10-30 15:32:55 -0300

    Realizado a criação do HTML e CSS da tela de recuperação de senha

--- src/app/LoginModule/login.module.ts
+++ src/app/LoginModule/login.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from "@angular/core";
 
 import { LoginComponent } from "./login/login.component";
 import { UserRegisterComponent } from './user-register/user-register.component';
+import { RecoverPasswordComponent } from './recover-password/recover-password.component';
 
 @NgModule({
 
     declarations: [  
         LoginComponent, 
-        UserRegisterComponent
+        UserRegisterComponent, 
+        RecoverPasswordComponent
     ], 
 
     exports: [
         LoginComponent,
-        UserRegisterComponent
+        UserRegisterComponent,
+        RecoverPasswordComponent
     ]
 })
 export class LoginModule{ }
